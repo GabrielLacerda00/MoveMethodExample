2384ee165de2ab4a526ac94177bb7d1277a0c977
GabrielLacerda00 <gabriel.lacerd.gr@gmail.com>
2023-06-05 11:14:41 -0300

    refactoring: MoveMethod

--- Operacao.java
+++ Operacao.java
@@ -8,4 +8,28 @@ public class Operacao {
     this.num1 = num1;
     this.num2 = num2;
   }
+
+  public int soma() {
+    return this.num1 + this.num2;
+  }
+
+  public int subtracao() {
+    return this.num1 - this.num2;
+  }
+
+  public int multiplicacao() {
+    return this.num1 * this.num2;
+  }
+
+  public int divisao() {
+    if (this.num2 != 0) {
+        return this.num1 / this.num2;
+    } else {
+        throw new IllegalArgumentException("Divisão por zero não é permitida");
+    }
+  }
+
+  public String getTipo() {
+    return tipo;
+  }
 }
