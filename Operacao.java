5d4d6f24a1631f5ced6258c991be5f762b44e064
GabrielLacerda00 <gabriel.lacerd.gr@gmail.com>
2023-06-05 10:54:32 -0300

    criando calculadora e operacao

---
+++ Operacao.java
@@ -0,0 +1,11 @@
+public class Operacao {
+  private String tipo;
+  private int num1;
+  private int num2;
+
+  public Operacao(String tipo, int num1, int num2) {
+    this.tipo = tipo;
+    this.num1 = num1;
+    this.num2 = num2;
+  }
+}
